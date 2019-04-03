9324f99d849ea1f78aa307392e7c493fd2c3a390
Michał Kordasz <1811552@gmail.com>
2019-04-03 17:21:52 +0200

    Pierwsza wersja

---
+++ ArduinoOswietlenie/ArduinoOswietlenie/ArduinoOswietlenie.ino
@@ -0,0 +1,16 @@
+/*
+ Name:		ArduinoOswietlenie.ino
+ Created:	4/3/2019 5:16:21 PM
+ Author:	18115
+*/
+
+// the setup function runs once when you press reset or power the board
+void setup() 
+{
+	pinMode(8, HIGH);
+}
+
+// the loop function runs over and over again until power down or reset
+void loop() {
+  
+}
